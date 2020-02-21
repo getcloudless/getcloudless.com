f47ba10d5f6e634fcb0d6235ebd63b6fe98eb071
Shaun Verch <shaun@getcloudless.com>
2020-02-21 15:07:07 -0500

    Remove any submodules and cloudless deployment
    Moving this to github pages because I'm no longer maintaining this
    iteration of the project.

--- deployment/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SERVICE_NAME="web-$(git rev-parse --short HEAD)"
-python example-static-site/helpers/deploy.py cloudless consul-1 "$SERVICE_NAME" \
-    getcloudless.com https://github.com/getcloudless/getcloudless.com/ "Cloudless"
